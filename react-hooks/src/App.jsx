00e890e02ca478b7ef51539f198056ecb0412d19
Justin <justinjenecke@gmail.com>
2024-02-14 14:29:53 +0200

    creates utils and maps over guesses

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css'
 import CounterObject from './components/2-CounterObject'
 import CounterPrevState from "./components/1-CounterPrevState"
+import LottoGame from "./components/3-Lotto-Game"
 
 function App() {
 
   return (
     <div className="App">
       {/* <CounterPrevState /> */}
-      <hr />
-      <CounterObject />
+      {/* <CounterObject /> */}
+      <LottoGame />
     </div>
   )
 }
--- react-hooks/src/components/3-Lotto-Game.jsx
+++ react-hooks/src/components/3-Lotto-Game.jsx
@@ -5,20 +5,32 @@ const LottoGame = () => {
     const [guesses, setGuesses] = useState([]);
 
     const generateNumber = () => {
+        return Math.floor(Math.random() * 10) + 1
+    }
 
+    const appendGuess = (newGuess) => {
+        setGuesses([...guesses, newGuess])
     }
 
     useEffect( () => {
         if (guesses.length === 3) {
             alert('game over')
-        }
+        } 
     }, [guesses])
 
     return (
         <section>
             <h2>Lotto Game</h2>
             <div className="guess-container">
-
+                <ul>
+                    {
+                        guesses.map( guess => {
+                            <li>
+                                {guess}
+                            </li>
+                        })
+                    }
+                </ul>
             </div>
             <div>
                 <input type="number" name="guess" id="guess"/>
@@ -27,3 +39,5 @@ const LottoGame = () => {
         </section>
     )
 }
+
+export default LottoGame
