596610a9dba364fe5351280b6219a0851d5a2664
Justin <justinjenecke@gmail.com>
2024-02-16 14:06:21 +0200

    changes guesses capacity to 7

--- react-hooks/src/components/3-Lotto-Game.jsx
+++ react-hooks/src/components/3-Lotto-Game.jsx
@@ -2,39 +2,41 @@ import { useEffect, useRef, useState } from "react";
 
 const LottoGame = () => {
 
+    const NUM_GUESSES = 7;
+
     const [guesses, setGuesses] = useState([]);
     const guessRef = useRef(0);
 
     const generateNumber = () => {
-        return Math.floor(Math.random() * 10) + 1
+        return Math.floor(Math.random() * 10) + 1;
     }
 
     const appendGuess = (newGuess) => {
-        setGuesses([...guesses, newGuess])
+        setGuesses([...guesses, newGuess]);
     }
 
     const guessClickHandler = (e) => {
-        const guess = document.querySelector('#guess')
+        const guess = document.querySelector('#guess');
         // console.log(guess.value)
-        appendGuess(parseInt(guess.value))
+        appendGuess(parseInt(guess.value));
     }
 
     useEffect( () => {
-        if (guesses.length === 3) {
+        if (guesses.length === NUM_GUESSES) {
 
-            const answer = 1
-            let winner = false
+            const answer = 1;
+            let winner = false;
 
             guesses.forEach(guess => {
               if(guess === answer)
-                winner = true
+                winner = true;
                 return
             });
 
             if(winner)  
-                alert(`congrats you won.. The lucky number was ${answer}`)
+                alert(`congrats you won.. The lucky number was ${answer}`);
             else
-                alert(`Game over... you lose. The lucky number was ${answer}`)
+                alert(`Game over... you lose. The lucky number was ${answer}`);
             
             setGuesses([])
         } 
@@ -44,7 +46,7 @@ const LottoGame = () => {
         <section>
             <h2>Lotto Game</h2>
             <div className="guess-container">
-                <ul>
+                <ol>
                     {
                         guesses.map( guess => {
                             return (
@@ -54,7 +56,7 @@ const LottoGame = () => {
                             )
                         })
                     }
-                </ul>
+                </ol>
             </div>
             <hr />
             <div>
