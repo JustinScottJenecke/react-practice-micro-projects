5348ab88accc723e1c2b9de9a8da4b00fc4da794
Justin <justinjenecke@gmail.com>
2024-02-16 20:13:06 +0200

    refactor: changes state name

--- react-hooks/src/components/7-EffectErrors.jsx
+++ react-hooks/src/components/7-EffectErrors.jsx
@@ -2,10 +2,10 @@ import { useEffect, useState } from "react";
 
 const EffectErrors = () => {
 
-    const [count, setCount] = useState(0)
+    const [timer, setTimer] = useState(0)
 
     const tick = () => {
-        setCount(prev => prev + 1)
+        setTimer(prev => prev + 1)
     }
 
     useEffect( () => {
@@ -18,7 +18,7 @@ const EffectErrors = () => {
 
     return ( 
         <>
-            {count}
+            {timer}
         </>
      );
 }
