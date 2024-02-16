2c4465b31cacdc416da6be2abf56a08a054c11a8
Justin <justinjenecke@gmail.com>
2024-02-16 18:23:27 +0200

    refactor: creates mouse handler function + removes effect on unmounting

--- react-hooks/src/components/5-MouseEffects.jsx
+++ react-hooks/src/components/5-MouseEffects.jsx
@@ -1,27 +1,36 @@
 import { useEffect, useState } from "react";
 
 const MouseEffects = () => {
-    
+
     const [x, setX] = useState(0);
     const [y, setY] = useState(0);
 
-    useEffect( () => {
+    const mouseMoveHandler = (e) => {
         
+        console.log('mouseEvent');
+        setX(e.clientX);
+        setY(e.clientY);
+
+    }
+
+    useEffect(() => {
+
         console.log('effect was triggered')
-        window.addEventListener('mousemove', (e) => {
-            console.log('mouseEvent');
-            setX(e.clientX);
-            setY(e.clientY);
-        })
+        window.addEventListener('mousemove', mouseMoveHandler) 
+
+        return () => {
+            console.log('component unmounting.. cleaning up');
+            window.removeEventListener('mousemove', mouseMoveHandler);
+        }
 
     }, [])
-    
-    return ( 
+
+    return (
         <section>
             <h2>Mouse Coordinates</h2>
             <b>X: {x}</b> | <b>Y: {y}</b>
         </section>
-     );
+    );
 }
 
-export default MouseEffects ;
+export default MouseEffects;
