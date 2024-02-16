8ac669e378f6d7c0be48782b43c9e82763ed10f2
Justin <justinjenecke@gmail.com>
2024-02-16 17:46:20 +0200

    updates title as side effect

--- react-hooks/src/components/4-CounterEffects.jsx
+++ react-hooks/src/components/4-CounterEffects.jsx
@@ -5,12 +5,16 @@ const CounterEffects = () => {
 
     const [count, setCount] = useState(0);
 
+    useEffect( () => {
+        document.title = `you clicked the count ${count} ${count == 1 ? 'time' : 'times'}`
+    })
+
     return (
         <div>
             <button onClick={() => {setCount(prevCount => prevCount + 1)}}>
                 you clicked {count} {count == 1 ? 'time' : 'times'}
             </button>
-        </div>
+        </div> 
     )
 }
 
