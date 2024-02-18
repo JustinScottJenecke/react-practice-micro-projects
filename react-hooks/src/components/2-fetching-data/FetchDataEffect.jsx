424d1ab307cbeaffc66a9fe2e82660607845e0d0
Justin <justinjenecke@gmail.com>
2024-02-18 18:49:12 +0200

    connects state to another state to pull data on btn click

--- react-hooks/src/components/2-fetching-data/FetchDataEffect.jsx
+++ react-hooks/src/components/2-fetching-data/FetchDataEffect.jsx
@@ -17,21 +17,51 @@ const FetchDataEffect = () => {
     }, [])
 
     // grab single post object
-    const [singlePost, setSinglePost] = useState();
+    const [post, setPost] = useState({});
+    const [idInput, setIdInput] = useState(1);
+    const [submittedId, setSubmittedId] = useState(1)
+
+    const handleGetPostButton = () => {
+        setSubmittedId(idInput)
+    }
+
+    useEffect( () => {
+        axios.get(`https://jsonplaceholder.org/posts/${submittedId}`)
+            .then(
+                response => {
+                    console.log(response.data)
+                    setPost(response.data)
+                }
+            )
+    }, [submittedId])
     
     return ( 
         <>
-            <ul>
-                {
-                    allPosts.map( singlePost => {
-                        return ( 
-                            <li key={singlePost.id}>
-                                {singlePost.title}
-                            </li>
-                        )
-                    })
-                }
-            </ul>
+            <div>
+                <h3>Single Post Display</h3>
+                <input type="number" onChange={ (e) => { setIdInput(e.target.value) }}/>
+                <button onClick={ handleGetPostButton }></button>
+                <ul>
+                    <li>{post.id}</li>
+                    <li>{post.slug}</li>
+                    <li>{post.title}</li>
+                </ul>
+            </div>
+            <hr /> 
+            <div>
+                <h3>All Posts</h3>
+                <ul>
+                    {
+                        allPosts.map( singlePost => {
+                            return ( 
+                                <li key={singlePost.id}>
+                                    {singlePost.title}
+                                </li>
+                            )
+                        })
+                    }
+                </ul>          
+            </div>
         </>
     );
 }
